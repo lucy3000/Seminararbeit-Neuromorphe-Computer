30bd2ce0bc7309a56c12c31b6482c8b6fea4f133
lucy3000 <164349783+lucy3000@users.noreply.github.com>
2024-10-18 16:28:24 +0200

    README

--- README.mdCatOrDogANE.py 	 Update CatOrDogANE.py 	 Oct 18, 2024 DogOrCat.py
+++ README.mdCatOrDogANE.py 	 Update CatOrDogANE.py 	 Oct 18, 2024 DogOrCat.py
@@ -5,4 +5,4 @@ Es gibt zwei Versionen des Programms, die auf verschiedene Prozessoren optimiert
 
     CatOrDogANE.py: Diese Version ist für Geräte mit einem Apple M2 Ultra Prozessor optimiert und nutzt die Apple Neural Engine (ANE) für eine bessere Leistung.
 
-    DogOrCat.py: Diese Version ist für Geräte mit einem Intel Core i5 Prozessor ausgelegt und nutzt die Standard-CPU-basierte Verarbeitung.
+    DogOrCat.py: Diese Version ist für Geräte mit einem Intel Core i5 Prozessor ausgelegt und nutzt die Standard-CPU-basierte Verarbeitung
