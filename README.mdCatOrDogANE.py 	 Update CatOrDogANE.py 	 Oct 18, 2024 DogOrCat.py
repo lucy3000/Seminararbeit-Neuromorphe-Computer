9075d60900f3f031241ad55670ed2b98da307fd0
lucy3000 <164349783+lucy3000@users.noreply.github.com>
2024-10-18 16:33:04 +0200

    Delete README.mdCatOrDogANE.py 	 Update CatOrDogANE.py 	 Oct 18, 2024 DogOrCat.py

--- README.mdCatOrDogANE.py 	 Update CatOrDogANE.py 	 Oct 18, 2024 DogOrCat.py
+++
@@ -1,8 +0,0 @@
-Um das Programm auszuführen, müssen Sie zuerst die erforderlichen Bibliotheken installieren und anschließend die Bilder von folgender URL herunterladen: 
-[https://www.microsoft.com/en-us/download/details.aspx?id=54765](https://www.microsoft.com/en-us/download/details.aspx?id=54765).
-
-Es gibt zwei Versionen des Programms, die auf verschiedene Prozessoren optimiert sind:
-
-    CatOrDogANE.py: Diese Version ist für Geräte mit einem Apple M2 Ultra Prozessor optimiert und nutzt die Apple Neural Engine (ANE) für eine bessere Leistung.
-
-    DogOrCat.py: Diese Version ist für Geräte mit einem Intel Core i5 Prozessor ausgelegt und nutzt die Standard-CPU-basierte Verarbeitung
